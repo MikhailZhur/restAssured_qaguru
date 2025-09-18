5264d4afd587df0579d4edd4888c69e0e2512bc7
MikhailZhur <miha99_66@mail.ru>
2025-09-18 23:27:06 +0500

    added 2 negative tests

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -1,22 +1,27 @@
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
 public class ReqresInTests {
 
+    public static String BASEURL = "https://reqres.in";
+    String requestLoginBody = "{\n" +
+            "    \"email\": \"eve.holt@reqres.in\",\n" +
+            "    \"password\": \"cityslicka\"\n" +
+            "}";
+
     @Test
     void listUsersCheckPage() {
-        get("https://reqres.in/api/users?page=2")
+        get(BASEURL + "/api/users?page=2")
                 .then()
                 .body("page", is(2));
     }
 
     @Test
     void listUsersCheckPageWithLogResponse() {
-        get("https://reqres.in/api/users?page=2")
+        get(BASEURL + "/api/users?page=2")
                 .then()
                 .log().all()
                 .body("page", is(2));
@@ -27,35 +32,82 @@ public class ReqresInTests {
         given()
                 .log().all()
                 .header("x-api-key", "reqres-free-v1")
-                .get("https://reqres.in/api/users?page=2")
+                .get(BASEURL + "/api/users?page=2")
                 .then()
                 .log().all()
                 .body("page", is(2));
     }
 
     @Test
-//    Делаем пост запрос /api/login с телом {
-//    "email": "eve.holt@reqres.in",
-//    "password": "pistol"
-//}
-//    проверяем что пришел токен
-
     void successfulLoginTest() {
         given()
                 .log().all()
                 .header("x-api-key", "reqres-free-v1")
-                .body("{\n" +
-                        "    \"email\": \"eve.holt@reqres.in\",\n" +
-                        "    \"password\": \"cityslicka\"\n" +
-                        "}")
-                .contentType(ContentType.JSON)
+                .body(requestLoginBody)
+                .contentType(JSON)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post(BASEURL + "/api/login")
                 .then()
                 .log().all()
                 .statusCode(200)
                 .body("token", is("QpwL5tke4Pnpja7X4"));
 
     }
+
+    @Test
+    void successfullRegTest() {
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .log().uri()
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"pistol\"\n" +
+                        "}")
+                .contentType(JSON)
+                .when()
+                .post(BASEURL + "/api/register")
+                .then()
+                .log().all()
+                .body("id",is(4),"token",is("QpwL5tke4Pnpja7X4"));
+
+    }
+
+    @Test
+    void fieldEmailIsRequiredWhenRegistering() {
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .log().uri()
+                .body("{\n" +
+                        "    \"email\": \"\",\n" +
+                        "    \"password\": \"pistol\"\n" +
+                        "}")
+                .contentType(JSON)
+                .when()
+                .post(BASEURL + "/api/register")
+                .then()
+                .log().all()
+                .body("error",is("Missing email or username"));
+
+    }
+
+    @Test
+    void fieldPasswordIsRequiredWhenRegistering() {
+        given()
+                .header("x-api-key", "reqres-free-v1")
+                .log().uri()
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"\"\n" +
+                        "}")
+                .contentType(JSON)
+                .when()
+                .post(BASEURL + "/api/register")
+                .then()
+                .log().all()
+                .body("error",is("Missing password"));
+
+    }
+
+
 
 }
